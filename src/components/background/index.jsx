9098ab11da6a69489d80942a73fb264a2d248fd7
kadir <develioglu306@gmail.com>
2022-04-18 20:57:03 +0300

    Add background svg.

--- src/components/background/index.jsx
+++ src/components/background/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { default as cn } from 'classnames';
 import style from './background.module.scss';
+import BackgroundSvg from 'assets/icons/Background.svg';
 export default function Background({ children, className }) {
-  return <div className={cn(style.background, className)}>{children}</div>;
+  return (
+    <div
+      className={cn(style.background, className)}
+      style={{
+        background: `url(${BackgroundSvg}), linear-gradient(63.43deg, #367d4d 0%, #00b0b9 50%, #82d89a 100%)`,
+        backgroundSize: 'cover',
+      }}
+    >
+      {children}
+    </div>
+  );
 }
