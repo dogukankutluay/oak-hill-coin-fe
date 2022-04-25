9d5f575de2a13411d1b62bc133e523a149ad1ee5
kadir <develioglu306@gmail.com>
2022-04-25 09:18:51 +0300

    add toggle navbar .

---
+++ src/assets/icons/header/index.jsx
@@ -0,0 +1,13 @@
+export const HamburgerMenu = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 17h-12v-2h12v2zm0-4h-12v-2h12v2zm0-4h-12v-2h12v2z" />
+    </svg>
+  );
+};
--- src/pages/home/components/header/index.jsx
+++ src/pages/home/components/header/index.jsx
@@ -1,11 +1,28 @@
 import { OakhillMainIcon } from 'assets/icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './header.module.scss';
 import { Link } from 'react-router-dom';
+import { HamburgerMenu } from 'assets/icons/header';
+import { default as cn } from 'classnames';
 export default function Header() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  });
   return (
     <header className={style.header}>
-      <ul className={style.header_items}>
+      {width <= 915 && (
+        <HamburgerMenu
+          className={style.header_hamburger}
+          onClick={() => setActive(!active)}
+        />
+      )}
+      <ul className={cn(style.header_items, active && style.dropdown)}>
         <li className={style.header_link}>
           <a href="#">
             <OakhillMainIcon />
@@ -39,6 +56,42 @@ export default function Header() {
           </Link>
         </li>
       </ul>
+      {active && (
+        <div
+          className={cn(
+            style.dropdown,
+            'animate__animated animate__fadeInLeft'
+          )}
+          onClick={() => setActive(false)}
+        >
+          <ul className={style.dropdown_menu}>
+            <li className={style.dropdown_item}>
+              <a href="#about">About Launchpad</a>
+            </li>
+            <li className={style.dropdown_item}>
+              <a href="#our-token">Our Token</a>
+            </li>
+            <li className={style.dropdown_item}>
+              <a href="#ico-schedule">ICO Schedule</a>
+            </li>
+            <li className={style.dropdown_item}>
+              <a href="#tokenomic">Tokenomics</a>
+            </li>
+            <li className={style.dropdown_item}>
+              <a href="#roadmap">Roadmap</a>
+            </li>
+            <li className={style.dropdown_item}>
+              <a href="#team">Team</a>
+            </li>
+            <li className={style.dropdown_item}>
+              <Link to={'/login'}>Login</Link>
+            </li>
+            <li className={style.dropdown_item}>
+              <Link to={'/register'}>Register</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
