b10986c11aa2c423d799a55d0c2abc500849c11e
kadir <develioglu306@gmail.com>
2022-05-07 11:16:24 +0300

    Change base url

--- src/constants/api/index.jsx
+++ src/constants/api/index.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8080/api/v1/';
+export const BASE_URL = 'https://radiant-eyrie-81265.herokuapp.com/api/v1/';
 const METHODS = {
   GET: 'GET',
   POST: 'POST',
